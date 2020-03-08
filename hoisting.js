add54f6defaf81b0f41830c5eec6e2a4668bf0e4
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-03-08 13:43:10 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,11 +11,13 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+  
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+
+  thisIsCrazy();
 }
 
 function sayMyName() {
